4fdf09b5bed0bb308d146c208d97505d86e68811
Klim <klim_somov@mail.ru>
2022-06-09 12:51:41 +0300

    rendering author, show more, title

--- src/componets/Story.jsx
+++ src/componets/Story.jsx
@@ -4,15 +4,14 @@ import { getStory } from "../services/api";
 const Story = ({ storyId }) => {
   const [story, setstory] = useState({});
   useEffect(() => {
-    getStory(storyId).then((data) =>data && data.url && setstory(data));
-   
+    getStory(storyId).then((data) => data && data.url && setstory(data));
   }, []);
 
   return (
     <div>
       <h2>{story.title}</h2>
-      <p>Автор: {story.by || "Неизвестен"}</p>
-      <a href={story.url}>читать полностью</a>
+      {story.by ? <p>Автор: {story.by}</p> : ""}
+      {story.url ? <a href={story.url}>читать полностью</a> : ""}
     </div>
   );
 };
